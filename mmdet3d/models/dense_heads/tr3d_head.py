(fix) fixed rotation and scaling

mmdet3d/models/dense_heads/tr3d_head.py
@@ -16,6 +16,11 @@ from mmdet.core.bbox.builder import BBOX_ASSIGNERS, build_assigner
 from physion.external.rotation_continuity.utils import compute_rotation_matrix_from_ortho6d
 from physion.physion_tools import PointCloudVisualizer, convert_to_world_coords_torch
 from physion.physion_nms import iou
+from torch.utils.tensorboard import SummaryWriter
+
+
+writer = SummaryWriter(log_dir="/home/kalyanav/MS_thesis/mmdetection3d/work_dirs/tr3d_physion_config/summary_logs")
+
 
 @HEADS.register_module()
 class TR3DHead(BaseModule):
@@ -53,11 +58,12 @@ class TR3DHead(BaseModule):
     def _forward_single(self, x):
 
         reg_final = self.bbox_conv(x).features
-        reg_distance = torch.exp(reg_final[:, 3:6])
+        # reg_distance = torch.exp(reg_final[:, 3:6])
+        reg_distance = reg_final[:, 3:6]
         
         #TODO: Need to fix this for accepting 6d 
         # reg_angle = reg_final[:, 6:]
-        reg_6d_ortho = torch.zeros_like(reg_final[:, 6:])
+        reg_6d_ortho = reg_final[:, 6:]
         bbox_pred = torch.cat((reg_final[:, :3], reg_distance, reg_6d_ortho), dim=1)
         cls_pred = self.cls_conv(x).features
 
@@ -195,7 +201,7 @@ class TR3DHead(BaseModule):
         bbox_preds = torch.cat(bbox_preds)
         cls_preds = torch.cat(cls_preds)
         points = torch.cat(points)
-
+        
         # cls loss
         n_classes = cls_preds.shape[1]
         pos_mask = assigned_ids >= 0
@@ -213,6 +219,13 @@ class TR3DHead(BaseModule):
             pos_bbox_preds = bbox_preds[pos_mask]
             bbox_targets = torch.cat((gt_bboxes.tensor[:, :3], gt_bboxes.tensor[:, 3:]), dim=1)
             pos_bbox_targets = bbox_targets.to(points.device)[assigned_ids][pos_mask]
+            
+            ######################################
+            # getting the center of pred and gt and append to the 8 corners
+            center_pos_bbox_preds = convert_to_world_coords_torch(pos_bbox_preds)[:, :1, :].to(points.device)
+            center_pos_bbox_targets = convert_to_world_coords_torch(pos_bbox_targets)[:, :1, :].to(points.device)
+            ######################################
+
             if pos_bbox_preds.shape[1] == 6:
                 pos_bbox_targets = pos_bbox_targets[:, :6]
 
@@ -220,14 +233,19 @@ class TR3DHead(BaseModule):
             #     self._bbox_to_loss(
             #         self._bbox_pred_to_bbox(pos_points, pos_bbox_preds)),
             #     self._bbox_to_loss(pos_bbox_targets)) 
+            # bbox_loss = self.bbox_loss(
+            #     self.bbox_to_corners(pos_bbox_preds) + pos_points.unsqueeze(1) , #TODO: test with the second term here
+            #     self.bbox_to_corners(pos_bbox_targets)
+            # )
             bbox_loss = self.bbox_loss(
-                self.bbox_to_corners(pos_bbox_preds) + pos_points.unsqueeze(1) , #TODO: test with the second term here
-                self.bbox_to_corners(pos_bbox_targets)
-            )
+            torch.concat((self.bbox_to_corners(pos_bbox_preds),center_pos_bbox_preds), dim=1),
+            torch.concat((self.bbox_to_corners(pos_bbox_targets),center_pos_bbox_targets), dim=1) 
+            )            
             # iou(self.bbox_to_corners(pos_bbox_preds),
             #     self.bbox_to_corners(pos_bbox_targets))           
         else:
             bbox_loss = None
+        
         return bbox_loss, cls_loss, pos_mask
 
     def _loss(self, bbox_preds, cls_preds, points,
@@ -401,7 +419,7 @@ class TR3DAssigner:
 
         # condition 2: keep topk location per box by center distance
         # center = boxes[..., :3] # n_points, n_boxes, 3
-        import pdb; pdb.set_trace()
+        
         #######################################################################
         # getting center from canonical coords
         center = world_coords_7_points[:, :1, :].squeeze(1).to(points.device).expand(n_points, n_boxes, 3)

mmdet3d/models/detectors/base.py
@@ -132,5 +132,5 @@ class Base3DDetector(BaseDetector):
                 file_name,
                 pred_corners=pred_corners,
                 gt_corners=bbox_to_corners(torch.from_numpy(gt_bbox)).cpu().numpy(),
-                show=show,
+                show=show, #TODO need to set this to show
                 pred_labels=pred_labels)

physion/physion_tools.py
@@ -561,16 +561,16 @@ def convert_to_world_coords_torch(gt_boxes_upright_depth_list):
                     "left":[-0.5, 0.5, 0],
                     "right":[0.5, 0.5, 0]}
     gt_world_coords = []
-    
+    dev = gt_boxes_upright_depth_list.device
     # Convert canonical values to torch tensors
-    canonical_values_torch = {key: torch.tensor(value, dtype=torch.float32) for key, value in canonical_values.items()}
+    canonical_values_torch = {key: torch.tensor(value, dtype=torch.float32, device=dev) for key, value in canonical_values.items()}
 
     for objects_info in gt_boxes_upright_depth_list:
         points_world_coord = []
         
         # Convert objects_info to numpy array if it's not already
         if not isinstance(objects_info, torch.Tensor):
-            objects_info = torch.Tensor(objects_info, dtype=torch.float32)
+            objects_info = torch.Tensor(objects_info, dtype=torch.float32, device=dev)
         
         # Convert objects_info to torch tensor
         # objects_info_torch = torch.Tensor(objects_info, dtype=torch.float32)